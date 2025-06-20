754d1da9f5009713c84ce2d1997a1c88ce997d96
DonatasNoreika <donoras@gmail.com>
2025-06-20 10:01:06 +0300

    +Įdėta straipsnių paieška

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, search
 
 urlpatterns = [
     path("", PostListView.as_view(), name='posts'),
     path("posts/<int:pk>", PostDetailView.as_view(), name="post"),
+    path('search/', search, name='search'),
 ]
--- mysite/myblog/views.py
+++ mysite/myblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.views import generic
+from django.db.models import Q
 
 
 # Create your views here.
@@ -17,3 +18,15 @@ class PostDetailView(generic.DetailView):
     context_object_name = "post"
 
 
+def search(request):
+    query = request.GET.get('query')
+
+    posts_search_results = Post.objects.filter(
+        Q(title__icontains=query) | Q(content__icontains=query) | Q(
+            author__first_name__icontains=query) | Q(author__last_name__icontains=query))
+
+    context = {
+        "query": query,
+        "posts": posts_search_results,
+    }
+    return render(request, template_name="search.html", context=context)
