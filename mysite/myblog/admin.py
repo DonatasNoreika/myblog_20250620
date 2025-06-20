2536bfb4726c50d953b8c9261f307c3a63072bf0
DonatasNoreika <donoras@gmail.com>
2025-06-20 08:44:44 +0300

    +Admin puslapyje leistų įvesti straipsnius (jie turi turėti pavadinimą, autorių, laiką ir tekstą).
    +Leistų prie įrašo parašyti komentarą (kol kas - tik per Admin puslapį). Komentaras turi turėti vartotojo vardą, el. paštą (nebūtina), laiką ir tekstą.

--- mysite/myblog/admin.py
+++ mysite/myblog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Post, Comment
+
 
 # Register your models here.
+
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'date', 'title']
+    inlines = [CommentInLine]
+
+
+admin.site.register(Post, PostAdmin)
--- mysite/myblog/models.py
+++ mysite/myblog/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(to=User, verbose_name="Autorius", on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
+    title = models.CharField(verbose_name="Pavadinimas", max_length=20)
+    content = models.TextField(verbose_name="Tekstas", max_length=3000)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(to="Post", verbose_name="Post", on_delete=models.CASCADE)
+    author = models.ForeignKey(to=User, verbose_name="Autorius", on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateTimeField(verbose_name="Data", auto_now_add=True)
+    content = models.TextField(verbose_name="Tekstas", max_length=1000)
