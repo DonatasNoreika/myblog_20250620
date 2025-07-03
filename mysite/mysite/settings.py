3e75ba46c10db6d94fd0d032cf8eb33cfbcc5ea5
DonatasNoreika <donoras@gmail.com>
2025-07-03 08:33:11 +0300

    +Įdėta crispy forma

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,8 @@ EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 INSTALLED_APPS = [
     'myblog',
     'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -161,3 +163,5 @@ TINYMCE_DEFAULT_CONFIG = {
     'menubar': True,
     'statusbar': True,
     }
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
